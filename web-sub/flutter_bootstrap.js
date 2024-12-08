840511a7915965e0eb0da5a059e44c447f55dbde
Mahdi Jamebozorg <mahdijamebozorg@email.com>
2024-12-08 13:15:23 +0330

    Add files via upload

--- web-sub/flutter_bootstrap.js
+++ web-sub/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2540871998"
+    serviceWorkerVersion: "1652799919"
   }
 });
