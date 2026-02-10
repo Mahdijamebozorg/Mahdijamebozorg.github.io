e9cf47aa2d744531eff315409a33cf095426bace
Mahdi Jamebozorg <mahdijamebozorg2000@gmail.com>
2026-02-10 11:49:34 +0330

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"587c18f873b8ab57330422bce09047420d9c7f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2754358057"
+    serviceWorkerVersion: "3283072815"
   }
 });
